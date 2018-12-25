dd84c094e4b7bd6fccd976158adc1c9e57809d74
Crazy <3346148964@qq.com>
2018-12-25 17:19:37 +0800

    2019年开始

---
+++ WPasswordProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WPasswordProject
+//
+//  Created by Crazy Wang on 2018/12/25.
+//  Copyright © 2018年 Crazy Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WPasswordProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WPasswordProject
+//
+//  Created by Crazy Wang on 2018/12/25.
+//  Copyright © 2018年 Crazy Wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
